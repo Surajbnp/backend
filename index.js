3617596889895568e97701db667ab86ceb207ecf
suraj <surajguptabnp14c@gmail.com>
2022-12-07 14:26:36 +0530

    upload

--- index.js
+++ index.js
@@ -9,18 +9,6 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  const timestamp = new Date().getTime();
-  // console.log(timestamp); // 👉️ 1642664853302
-  const date = new Date(timestamp);
-  let d3 =  new Date(timestamp)
-  let d1 = date.toLocaleString("sv")
-  // console.log(date.toLocaleString("sv"));
-  date.setDate(date.getDate() + 1);
-  // console.log(date.toLocaleString("sv"));
-  let d2 = date.toLocaleString("sv")
-
-  console.log(d1 === d3.toLocaleString("sv"));
-
   res.send("HomePage");
 });
 
